388b5a00febf6c091a8d9dddb31da27ef8a000f8
Naga Kavya <nagakavyakakumanu@gmail.com>
2025-12-29 23:57:16 +0530

    retriving the data and CRUD operations

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 app=FastAPI()
 
 class Post(BaseModel):
@@ -9,20 +10,45 @@ class Post(BaseModel):
   content:str
   published:bool=True
   rating:Optional[int]=None
+  
+my_posts=[{"title":"title of the post 1","content":"content of post 1","id":1},
+          {"title":"favorite food","content":"I like Pizza","id":2}]
+
+def find_post(id):
+  for p in my_posts:
+    if p['id'] == id:
+      return p
+    
+
 
 @app.get("/")
 def get_posts():
-  return {"data" : "this is your posts"}
+  return {"dat" : "this is your posts"}
+
 @app.get("/posts")
-async def root():
-  return {"message":"Welcome to my API!!!"}
+def get_posts():
+  return {"data":my_posts}
 
-@app.post("/createposts")
-def create_posts(post:Post):   #referencing pydantic model
-  print(post.rating)
-  print(post.dict())   #given all values in dict
-  return {"data":post}
+@app.post("/posts")
+def create_posts(post:Post):     #referencing pydantic model
+  # instead of dict we use model_dump in pydantic
+  post_dict=post.model_dump()    
+  post_dict['id']=randrange(0, 1000000)
+  my_posts.append(post_dict)
+  return {"data":post_dict}      
+  #it separately gives random is=d to every post inserted into it
+  #thus by running get method we get all the posts send to the post_dict
+    #given all values in dict
 
-#title str, content str
+@app.get("/posts/{id}")    #id represents the path parameter id of specific post
+def get_post(id: str):      #automatically converts the the inbuilt string into int
+  post=find_post(id)
+  print(post)
+  return {"post_detail":post}
+  
+  
+      
+  
+  
           
 
